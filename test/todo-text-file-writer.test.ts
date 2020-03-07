6f47983dbaf44245a4d93d3bf6f2aebec03c7cef
Chinedu Eze <ochuzor@gmail.com>
2020-03-07 15:17:49 +0200

    added test for text file writer

---
+++ test/todo-text-file-writer.test.ts
@@ -0,0 +1,19 @@
+import {TodoTextFileWriter} from '../src/text-file-exporters/todo-file-writers'
+
+describe('TodoTextFileWriter:', () => {
+    const filePath = 'file-path';
+    const mockWriter = {
+        writeFileSync: jest.fn(),
+    };
+    const data = 'data string';
+    let Sut: TodoTextFileWriter;
+
+    beforeEach(() => {
+        Sut = new TodoTextFileWriter(filePath, mockWriter);
+        Sut.write(data);
+    });
+
+    it('should call writeFileSync', () => {
+        expect(mockWriter.writeFileSync).toHaveBeenCalledWith(filePath, data, 'utf8');
+    });
+});
