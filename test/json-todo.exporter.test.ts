7ed5d281a613a62244e060799af26b53adb91e09
Chinedu Eze <ochuzor@gmail.com>
2020-03-07 14:44:31 +0200

    refactored json todo exporter tests
    added more

--- test/json-todo.exporter.test.ts
+++ test/json-todo.exporter.test.ts
@@ -1,49 +1,72 @@
 import { JsonTodoExporter } from '../src/string-format-exporters/json-todo.exporter';
 
 describe('JsonTodoExporter:', () => {
-    let Sut: JsonTodoExporter;
+    describe('with default JSON.stringify:', () => {
+        let Sut: JsonTodoExporter;
 
-    beforeEach(() => {
-        Sut = new JsonTodoExporter();
+        beforeEach(() => {
+            Sut = new JsonTodoExporter();
+        });
+
+        it('export: should export and empty list', () => {
+            const result = Sut.export([]);
+            expect(result).toEqual('[]');
+        });
+
+        it('export: should export a list of one item with empty text', () => {
+            const data = [{ id: 1, text: '' }];
+            const result = Sut.export(data);
+            expect(result).toEqual('[{"id":1,"text":""}]');
+        });
+
+        it('export: should export a list of one item', () => {
+            const data = [{ id: 1, text: 'x close the deal' }];
+            const result = Sut.export(data);
+            expect(result).toEqual('[{"id":1,"text":"x close the deal"}]');
+        });
+
+        it('export: should export a list of two items', () => {
+            const data = [
+                { id: 1, text: 'x close the deal' },
+                { id: 2, text: '(A) check the latest code' },
+            ];
+            const result = Sut.export(data);
+            expect(result).toEqual(
+                '[{"id":1,"text":"x close the deal"},{"id":2,"text":"(A) check the latest code"}]'
+            );
+        });
+
+        it('export: should export a list of three items', () => {
+            const data = [
+                { id: 1, text: 'x close the deal' },
+                { id: 2, text: '(A) check the latest code' },
+                { id: 3, text: '(C) practice for the test @home' },
+            ];
+            const result = Sut.export(data);
+            expect(result).toEqual(
+                '[{"id":1,"text":"x close the deal"},{"id":2,"text":"(A) check the latest code"},{"id":3,"text":"(C) practice for the test @home"}]'
+            );
+        });
     });
 
-    it('export: should export and empty list', () => {
-        const result = Sut.export([]);
-        expect(result).toEqual('[]');
-    });
-
-    it('export: should export a list of one item with empty text', () => {
-        const data = [{ id: 1, text: '' }];
-        const result = Sut.export(data);
-        expect(result).toEqual('[{"id":1,"text":""}]');
-    });
-
-    it('export: should export a list of one item', () => {
+    describe('with mock stringify:', () => {
+        let Sut: JsonTodoExporter;
         const data = [{ id: 1, text: 'x close the deal' }];
-        const result = Sut.export(data);
-        expect(result).toEqual('[{"id":1,"text":"x close the deal"}]');
-    });
+        const mockJson = JSON.stringify(data);
+        const mockStringify = jest.fn().mockReturnValue(mockJson);
+        let result: string;
 
-    it('export: should export a list of two items', () => {
-        const data = [
-            { id: 1, text: 'x close the deal' },
-            { id: 2, text: '(A) check the latest code' },
-        ];
-        const result = Sut.export(data);
-        expect(result).toEqual(
-            '[{"id":1,"text":"x close the deal"},{"id":2,"text":"(A) check the latest code"}]'
-        );
-    });
+        beforeEach(() => {
+            Sut = new JsonTodoExporter(mockStringify);
+            result = Sut.export(data);
+        });
 
-    it('export: should export a list of three items', () => {
-        const data = [
-            { id: 1, text: 'x close the deal' },
-            { id: 2, text: '(A) check the latest code' },
-            { id: 3, text: '(C) practice for the test @home' },
-        ];
-        const result = Sut.export(data);
-        expect(result).toEqual(
-            '[{"id":1,"text":"x close the deal"},{"id":2,"text":"(A) check the latest code"},{"id":3,"text":"(C) practice for the test @home"}]'
-        );
+        it('export: should call stringify', () => {
+            expect(mockStringify).toHaveBeenCalledWith(data);
+        });
+
+        it('...with the right data', () => {
+            expect(result).toEqual(mockJson);
+        });
     });
 });
